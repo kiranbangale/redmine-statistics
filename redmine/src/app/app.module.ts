0e504d07aef8c18445092434f43e4aff133c020a
Kiran Bangale <kiranbangale@gmail.com>
2018-10-29 09:16:52 +0530

    Added app structure

--- redmine/src/app/app.module.ts
+++ redmine/src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { AuthComponent } from './auth/auth.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { BaseComponent } from './base/base.component';
+import { HeaderComponent } from './base/header/header.component';
+import { FooterComponent } from './base/footer/footer.component';
+import { MenuComponent } from './base/menu/menu.component';
+import { DashboardComponent } from './base/dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    RegisterComponent,
+    LoginComponent,
+    BaseComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    }),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
